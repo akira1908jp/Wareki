e3d196e9d42e80e15d3c046f7da601b586eadf08
akira1908jp <akira1908jp@gmail.com>
2019-03-28 18:17:40 +0900

    add src
    source

---
+++ src/Wareki.php
@@ -0,0 +1,53 @@
+<?php
+namespace Wareki;
+use \DateTime;
+/**
+ * @author akira1908jp
+ * 和暦表示
+ */
+class Wareki {
+
+    public const GENGOU_MEIJI = "明治";
+
+    public const GENGO_TAISYOU = "大正";
+
+    Public const GENGOU_SYOUWA = "昭和";
+
+    public const GENGOU_HEISEI = "平成";
+
+    private const MEIJI_DATE = "1868-01-25";
+
+    private const TAISYOU_DATE = "1912-07-30";
+
+    private const SYOUWA_DATE = "1926-12-25";
+
+    private const HEISEI_DATE = "1989-01-08";
+
+    /**
+     * @DateTime
+     */
+    public function convertWareki($date) {
+
+        if ($date >= new DateTime(self::HEISEI_DATE)) {
+            $gengo = self::GENGOU_HEISEI;
+            $wayear = $date->format('Y') - 1988;
+        } elseif ($date >= new DateTime(self::SYOUWA_DATE)) {
+            $gengo = self::GENGOU_SYOUWA;
+            $wayear = $date->format('Y') - 1925;
+        } elseif ($date >= new DateTieme(self::TAISYOU_DATE)) {
+            $gengo = self::GENGO_TAISYOU;
+            $wayear = $date->format('Y') - 1911;
+        } elseif ($date >= new DateTime(self::MEIJI_DATE)) {
+            $gengo = self::GENGOU_MEIJI;
+            $wayear = $date->format('Y') - 1868;
+        }    
+        switch ($wayear) {
+            case 1:
+                $wadate = $gengo.'元年';
+                break;
+            default:
+                $wadate = $gengo.sprintf("%02d年", $wayear);
+        }
+        return $wadate.$date->format("m月d日");
+    }
+}
---
+++ tests/WarekiTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'vendor/autoload.php';
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * wareki test
+ */
+class WarekiTest extends TestCase {
+
+    /**
+     * test 平成
+     */
+    public function testHeisei() {
+        $warekiDate = new Wareki\Wareki();
+        $HeiseiDate = $warekiDate->convertWareki(new DateTime("1989-01-09"));
+        $this->assertEquals($HeiseiDate, "平成元年01月09日");
+        $HeiseiDate = $warekiDate->convertWareki(new DateTime("1990-01-01"));
+        $this->assertEquals($HeiseiDate, "平成02年01月01日");
+    }
+
+    /**
+     * test 昭和
+     */
+    public function testSyouwa() {
+        $warekiDate = new Wareki\Wareki();
+        $HeiseiDate = $warekiDate->convertWareki(new DateTime("1926-12-25"));
+        $this->assertEquals($HeiseiDate, "昭和元年12月25日");
+        $HeiseiDate = $warekiDate->convertWareki(new DateTime("1927-12-25"));
+        $this->assertEquals($HeiseiDate, "昭和02年12月25日");
+    }
+
+}
